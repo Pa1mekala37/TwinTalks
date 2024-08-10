a6df705bb76460ae02f7d7ebf42baffd6a11589d
PAVAN MEKALA <pavanmekala64@gmail.com>
2024-08-10 07:08:35 +0000

    🔄 frontend server url updated

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect(process.env.MONGO_DB_URI);
-		console.log("Connected to MongoDB");
+		await mongoose.connect(process.env.MONGO_DB_URL);
+		console.log("Server Connected to Database 🚀");
 	} catch (error) {
-		console.log("Error connecting to MongoDB", error.message);
+		console.log("Error connecting to Database", error.message);
 	}
 };
 
