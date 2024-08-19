93e07f2dc6ba756ddedbf27720ffb69064fe7522
PAVAN MEKALA <pavanmekala64@gmail.com>
2024-08-19 19:31:12 +0530

    update socket.io url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -1,31 +1,9 @@
 import { createContext, useState, useEffect, useContext } from "react";
 import { useAuthContext } from "./AuthContext";
 import io from "socket.io-client";
-
 const SocketContext = createContext();
-
 export const useSocketContext = () => {
 	return useContext(SocketContext);
-};
-
-const connectSocket = (urls, query) => {
-	for (const url of urls) {
-		const socket = io(url, { query });
-
-		// Listen for connection event to see if the socket successfully connects
-		socket.on("connect", () => {
-			console.log(`Connected to socket server at ${url}`);
-			return socket;
-		});
-
-		// Listen for connection error and attempt next URL if connection fails
-		socket.on("connect_error", (error) => {
-			console.error(`Failed to connect to ${url}: ${error.message}`);
-			socket.close();
-		});
-	}
-
-	return null;
 };
 
 export const SocketContextProvider = ({ children }) => {
@@ -35,25 +13,20 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const urls = [
-				"https://twintalks.up.railway.app", // Primary (Production)
-				"https://twintalks.onrender.com",  // Secondary (Staging)
-			];
+			const socket = io("https://twintalks.up.railway.app", {
+				query: {
+					userId: authUser._id,
+				},
+			});
 
-			const newSocket = connectSocket(urls, { userId: authUser._id });
-			setSocket(newSocket);
+			setSocket(socket);
 
-			if (newSocket) {
-				newSocket.on("getOnlineUsers", (users) => {
-					setOnlineUsers(users);
-				});
-			}
+			// socket.on() is used to listen to the events. can be used both on client and server side
+			socket.on("getOnlineUsers", (users) => {
+				setOnlineUsers(users);
+			});
 
-			return () => {
-				if (newSocket) {
-					newSocket.close();
-				}
-			};
+			return () => socket.close();
 		} else {
 			if (socket) {
 				socket.close();
@@ -62,9 +35,5 @@ export const SocketContextProvider = ({ children }) => {
 		}
 	}, [authUser]);
 
-	return (
-		<SocketContext.Provider value={{ socket, onlineUsers }}>
-			{children}
-		</SocketContext.Provider>
-	);
+	return <SocketContext.Provider value={{ socket, onlineUsers }}>{children}</SocketContext.Provider>;
 };
